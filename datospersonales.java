795c4921b0d62e162a6302f735cfc08531122301
federico <fdealmeida@americavirtualsa.com>
2023-05-05 12:33:53 -0300

    datos personales tarea

---
+++ datospersonales.java
@@ -0,0 +1,16 @@
+public class datospersonales {
+
+public static void main(String[] args) {
+    String nombre = "Federico ";
+
+    String apellido = "De Almeida";
+
+     int    edad = 20;
+
+    String domicilio = "Jovellanos ";
+
+    System.out.println("mi nombre es " + nombre + "y me apellido " + apellido + " tengo " + edad + " años" + ",vivo en " + domicilio);
+      
+}
+    
+}
