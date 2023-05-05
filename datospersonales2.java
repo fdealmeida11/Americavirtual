0305517b049bb1f8cc3900e4e3a6356cee5f192d
fdealmeida11 <132680088+fdealmeida11@users.noreply.github.com>
2023-05-05 13:53:39 -0300

    Add files via upload

---
+++ datospersonales2.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class datospersonales2 {
+    public static void main(String[] args) {
+        String nombre = JOptionPane.showInputDialog("¿Cual es tu nombre?");
+        String apellido = JOptionPane.showInputDialog("¿y tu apellido?");
+        int edad = Integer.parseInt(JOptionPane.showInputDialog("¿Cuantos años tenes?"));
+        String domicilio = JOptionPane.showInputDialog("¿Donde vivis?");
+        JOptionPane.showMessageDialog(null, "Hola soy " + nombre + apellido + " tengo " + edad + " años y vivo en la calle " + domicilio);
+    }
+}
