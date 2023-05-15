1efbb6b3afc48bc3ecf18dc9c348fa250004d1ba
fdealmeida11 <132680088+fdealmeida11@users.noreply.github.com>
2023-05-15 13:13:26 -0300

    Add files via upload

--- Projecto 1/projecto1/src/ProjectoOrdenDeCompra.java
+++ Projecto 1/projecto1/src/ProjectoOrdenDeCompra.java
@@ -4,7 +4,7 @@ public class ProjectoOrdenDeCompra {
 
     public static void main(String [] args){
 
-        ordenCompra comp1 = new ordenCompra(1,"es una caja",24,3,"caja","jorge");
+   
 
     }
  }
